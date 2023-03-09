6313e1f7816470a025e1aaaacace08c5a799e26c
Adam Skoczeń <adamsk91@wp.pl>
2023-03-09 20:52:41 +0100

    123

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const imgSrc = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+
+const Product = props => (
+  <div>
+    <img
+      src={imgSrc}
+      alt="Tacos With Lime"
+      width="640"
+    />
+    <h2>Tacos With Lime</h2>
+    <p>Price: 10.99$</p>
+    <button type="button">Add to cart</button>
+  </div>
 );
+
+// Chcąc wykorzystać nasz komponent zapisujemy go jako tag JSX
+ReactDOM.createRoot(document.getElementById("root")).render(<Product />);
