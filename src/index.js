12fd6d14dc47a5ac09f7da1ffae58f184c6ede6b
Adam Skoczeń <adamsk91@wp.pl>
2023-03-12 08:43:52 +0100

    1

--- src/index.js
+++ src/index.js
@@ -2,21 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { App } from 'components/App';
 import './index.css';
+// import PropTypes from "prop-types";
 
-const imgSrc = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" },
+  { id: "id-4", name: "Redux in depth" },
+];
 
-const Product = props => (
-  <div>
-    <img
-      src={imgSrc}
-      alt="Tacos With Lime"
-      width="640"
-    />
-    <h2>Tacos With Lime</h2>
-    <p>Price: 10.99$</p>
-    <button type="button">Add to cart</button>
-  </div>
+const BookList = ({ books }) => (
+  <ul>
+    {books.map((book, index) => (
+      <li key={index}>{book.name}</li>
+    ))}
+  </ul>
 );
 
+const App = () => {
+  return (
+    <div>
+      <BookList books={favouriteBooks} />
+    </div>
+  );
+};
+
 // Chcąc wykorzystać nasz komponent zapisujemy go jako tag JSX
-ReactDOM.createRoot(document.getElementById("root")).render(<Product />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
