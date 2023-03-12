ab47becf7eb80091e474c5c1434d80c5b0ebe34d
Adam Skoczeń <adamsk91@wp.pl>
2023-03-12 12:06:39 +0100

    1

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import clsx from "clsx";
 // import { App } from 'components/App';
 import './index.css';
 // import PropTypes from "prop-types";
 
-const favouriteBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" },
-  { id: "id-4", name: "Redux in depth" },
-];
+import css from "./Style.module.css";
 
-const BookList = ({ books }) => (
-  <ul>
-    {books.map((book, index) => (
-      <li key={index}>{book.name}</li>
-    ))}
-  </ul>
-);
+const Alert = ({ variant, outlined, elevated, children }) => {
+  return <p className={clsx(
+    css[variant], {
+      [css.isOutlined]: outlined,
+      [css.isElevated]: elevated
+  })
+  }>{children}</p>;
+};
 
 const App = () => {
   return (
-    <div>
-      <BookList books={favouriteBooks} />
-    </div>
+    <>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error" outlined>
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success" elevated>
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning" outlined elevated>
+        Please update your profile contact information
+      </Alert>
+    </>
   );
 };
 
