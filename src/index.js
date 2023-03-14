2dc70cb1394c2699d8332365a1b9e8dcd475fc9b
Adam Skoczeń <adamsk91@wp.pl>
2023-03-14 19:42:36 +0100

    exe 1 complete

--- src/index.js
+++ src/index.js
@@ -1,36 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import clsx from "clsx";
-// import { App } from 'components/App';
-import './index.css';
+import { Profile } from 'components/profile/Profile';
+import user from "./user.json";
+// import clsx from "clsx";
 // import PropTypes from "prop-types";
-
-import css from "./Style.module.css";
-
-const Alert = ({ variant, outlined, elevated, children }) => {
-  return <p className={clsx(
-    css[variant], {
-      [css.isOutlined]: outlined,
-      [css.isElevated]: elevated
-  })
-  }>{children}</p>;
-};
 
 const App = () => {
   return (
     <>
-      <Alert variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert variant="error" outlined>
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success" elevated>
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning" outlined elevated>
-        Please update your profile contact information
-      </Alert>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </>
   );
 };
