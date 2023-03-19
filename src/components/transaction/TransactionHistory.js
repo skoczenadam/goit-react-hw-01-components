75b5d0129b78e87e2556c25955d61a490e97e4e6
Adam Skoczeń <adamsk91@wp.pl>
2023-03-19 12:35:31 +0100

    fixed

--- src/components/transaction/TransactionHistory.js
+++ src/components/transaction/TransactionHistory.js
@@ -1,8 +1,6 @@
 import css from "./TransactionHistory.module.css";
 import PropTypes from "prop-types";
 
-let even = 1;
-
 export const TransactionHistory = ({ items }) => {
   return (
     <table className={css.transactionHistory}>
@@ -14,12 +12,11 @@ export const TransactionHistory = ({ items }) => {
         </tr>
       </thead>
       <tbody>
-        {items.map(item => (
-          even++,
+        {items.map((item, index) => (
           <tr key={item.id}>
-            <td className={even%2 && css.even}>{item.type}</td>
-            <td className={even%2 && css.even}>{item.amount}</td>
-            <td className={even%2 && css.even}>{item.currency}</td>
+            <td className={index%2 && css.even}>{item.type}</td>
+            <td className={index%2 && css.even}>{item.amount}</td>
+            <td className={index%2 && css.even}>{item.currency}</td>
           </tr>
         ))}
       </tbody>
